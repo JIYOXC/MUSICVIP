d5c30b3ddba4f647f5b8abc547fccdfb6813e408
jafarpur2023 <160336468+jafarpur2023@users.noreply.github.com>
2024-04-22 16:58:12 +0330

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -12,13 +12,13 @@ def start_panel(_):
             ),
         ],
         [
-            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="۞ Help ۞", callback_data="settings_back_helper"),
             InlineKeyboardButton(
                 text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"
             ),
         ],
         [
-            InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="✡ گروه ✡", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -28,16 +28,16 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
+                text="💠 منو به گروه جدیدت اضافه بکن 💠",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text="𝐆𝚁𝙾𝚄𝙿✨", url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text="𝐌ᴏʀᴇ🥀", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="گروه✨", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="درباره ما🥀", url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper")
+            InlineKeyboardButton(text="۞ ویژگی ها ۞", callback_data="settings_back_helper")
         ],
     ]
     return buttons
